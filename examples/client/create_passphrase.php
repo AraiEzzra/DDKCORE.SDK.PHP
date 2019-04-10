49ee4d19e5e3c2b5afab6546f4bbfaea6a4dbb7e
Dmytro Mehed <d.mehed@sk-consulting.com.ua>
2019-04-10 09:35:28 +0300

    add function makeKeyPair and update example createPassphrase

--- examples/client/create_passphrase.php
+++ examples/client/create_passphrase.php
@@ -4,5 +4,7 @@ require __DIR__ . '/../bootstrap.php';
 
 
 $passpharse = $sdk->createPasspharse();
+$keyPair = \DDK\Crypto\KeyPair::makeKeyPair($passpharse);
 
 var_dump($passpharse);
+var_dump($keyPair);
--- src/DDK/Crypto/KeyPair.php
+++ src/DDK/Crypto/KeyPair.php
@@ -2,9 +2,11 @@
 
 namespace DDK\Crypto;
 
-
 class KeyPair
 {
+    const CRYPTO_SIGN_SECRETKEYBYTES = 64;
+    const CRYPTO_SIGN_PUBLICKEYBYTES = 64;
+
     public static function makePublickey (string $secret)
     {
         $keypair = sodium_crypto_sign_keypair();
@@ -12,5 +14,14 @@ class KeyPair
         $publickey = sodium_crypto_sign_publickey($keypair);
         $hash = sodium_crypto_generichash($secret, $publickey);
         return bin2hex($hash);
+    }
+
+    public static function makeKeyPair (string $secret)
+    {
+        $binKeyPair = sodium_crypto_sign_seed_keypair(hash('sha256', $secret, true));
+        $hexKeyPair = bin2hex($binKeyPair);
+        return array('publicKey' => substr($hexKeyPair, 0, self::CRYPTO_SIGN_SECRETKEYBYTES),
+        'secretKey' => substr($hexKeyPair, self::CRYPTO_SIGN_SECRETKEYBYTES, self::CRYPTO_SIGN_PUBLICKEYBYTES)
+        );
     }
 }
